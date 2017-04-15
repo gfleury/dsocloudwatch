560b5f3a4097b58cdd9e6c169c6e6f0ebd2dca95
gfleury <gfleury@ip-172-31-13-97.us-west-2.compute.internal>
2017-04-15 21:31:39 +0000

    ident

--- put_metric.cpp
+++ put_metric.cpp
@@ -13,33 +13,33 @@ void init_aws_sdk() {
 
 int put_metric_cw(char *nameSpace, char *metricName, char *dimensionName, char *dimensionValue, int value, const char *errMsg) {
 
-Aws::CloudWatch::Model::Dimension dimension;
-dimension.SetName(dimensionName);
-dimension.SetValue(dimensionValue);
+	Aws::CloudWatch::Model::Dimension dimension;
+	dimension.SetName(dimensionName);
+	dimension.SetValue(dimensionValue);
 
-Aws::CloudWatch::Model::MetricDatum datum;
-datum.SetMetricName(metricName);
-datum.SetUnit(Aws::CloudWatch::Model::StandardUnit::None);
-datum.SetValue(value);
-datum.AddDimensions(dimension);
+	Aws::CloudWatch::Model::MetricDatum datum;
+	datum.SetMetricName(metricName);
+	datum.SetUnit(Aws::CloudWatch::Model::StandardUnit::None);
+	datum.SetValue(value);
+	datum.AddDimensions(dimension);
 
-Aws::CloudWatch::Model::PutMetricDataRequest request;
-request.SetNamespace(nameSpace);
-request.AddMetricData(datum);
+	Aws::CloudWatch::Model::PutMetricDataRequest request;
+	request.SetNamespace(nameSpace);
+	request.AddMetricData(datum);
 
-auto outcome = cw.PutMetricData(request);
-if (!outcome.IsSuccess()) {
-    	std::cout << "Failed to put sample metric data:" <<
-        outcome.GetError().GetMessage() << std::endl;
-	if (errMsg)
-		errMsg = outcome.GetError().GetMessage().c_str();
-	return 1;
-} else {
-	std::cout << "Successfully put sample metric data" << std::endl;
-	return 0;
-}
+	auto outcome = cw.PutMetricData(request);
+	if (!outcome.IsSuccess()) {
+  	  	std::cout << "Failed to put sample metric data:" <<
+	        outcome.GetError().GetMessage() << std::endl;
+		if (errMsg)
+			errMsg = outcome.GetError().GetMessage().c_str();
+		return 1;
+	} else {
+		std::cout << "Successfully put sample metric data" << std::endl;
+		return 0;
+	}
 }
 
 void shutdown_aws_sdk() {
-    Aws::ShutdownAPI(options);
+	Aws::ShutdownAPI(options);
 }
